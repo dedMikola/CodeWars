271a45a26f33d3a4eba63c20c75c07a47a43ff24
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2022-01-27 11:33:14 +0200

    Update Duplicate Encoder.js

--- Duplicate Encoder.js
+++ Duplicate Encoder.js
@@ -4,18 +4,15 @@
 
 // ### Examples
 
-
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
 // "(( @"     =>  "))((" 
 
-
 // ### Notes
 
 // Assertion messages may be unclear about what they display in some languages.
 // If you read `"...It Should encode XXX"`, the `"XXX"` is the expected result, not the input!
-
 
 
 function duplicateEncode(word){
