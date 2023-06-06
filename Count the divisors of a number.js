5abf028aab6fc5ab144a26552f53557bd91d35d8
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2023-06-06 10:39:32 +0400

    Create Count the divisors of a number.js

---
+++ Count the divisors of a number.js
@@ -0,0 +1,35 @@
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+
+
+const getDivisorsCnt = (num = 1) => {  
+    if (num === 1) return num
+  
+    let divArr = [[2, 0]] 
+    let div = divArr[0][0] 
+  
+    while (num > 1) {
+      if (num % div === 0) {
+        for (let i = 0; divArr.length; i++) {
+          if (divArr[i][0] === div) {
+            divArr[i][1] += 1
+            break
+          } else {
+            if (i === divArr.length - 1) {
+              divArr.push([div, 1])
+              break
+            }
+          }
+        }
+        num /= div
+      } else {
+        div += 1
+      }
+    }
+    
+    for (let i = 0; i < divArr.length; i++) {
+      num *= divArr[i][1] + 1
+    }
+    
+    return num
+}
