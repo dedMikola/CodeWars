5fb7091466fda35a6a12128f6b0b653169b30541
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2023-06-10 22:50:59 +0400

    Create [6kyu] Unique In Order.js

---
+++ [6kyu] Unique In Order.js
@@ -0,0 +1,28 @@
+// Implement the function unique_in_order which takes as argument a sequence
+// and returns a list of items without any elements with the same value next
+// to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+let uniqueInOrder = function (iterable) {
+    let newArr = [];
+    if (Array.isArray(iterable)) {
+        iterable.map((item) => {
+            if (String(item) !== String(newArr.slice(-1))) {
+                newArr.push(item);
+            }
+        })
+    } else {
+        String(iterable).split("").map((item) => {
+            if (String(item) !== String(newArr.slice(-1))) {
+                newArr.push(item)
+            }
+        })
+    }
+    return newArr
+}
