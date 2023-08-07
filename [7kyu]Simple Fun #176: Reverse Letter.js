4dbe578b11a7161baa7d679d607c6dca4501855d
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2023-08-07 20:55:43 +0400

    Create [7kyu]Simple Fun #176: Reverse Letter.js

---
+++ [7kyu]Simple Fun #176: Reverse Letter.js
@@ -0,0 +1,23 @@
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+
+
+function reverseLetter(str) {
+    let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    return str.split('').filter(item =>{
+        return arr_en.includes(item)
+    }).reverse().join('')
+}
