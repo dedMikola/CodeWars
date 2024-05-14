30267dd1c6b65c8b75aaea7e675cf6b21eddf0c7
Sergey Nikolaev <73792465+dedMikola@users.noreply.github.com>
2024-05-14 18:44:31 +0200

    Update Array plus array.js

--- Array plus array.js
+++ Array plus array.js
@@ -1,6 +1,6 @@
-I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
 
-P.S. Each array includes only integer numbers. Output is a number too.
+// P.S. Each array includes only integer numbers. Output is a number too.
 
 
   let arrayPlusArray = (arr1, arr2) => 
